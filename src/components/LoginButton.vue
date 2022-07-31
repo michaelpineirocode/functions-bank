5b0ebfccbd224bdd461d5d59de65f81e41c377c3
Michael Pineiro <michaelpineiro.code@gmail.com>
2022-07-31 14:32:09 -0600

    fully initialized firebase

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -19,7 +19,6 @@ export default {
   },
   methods: {
     login() {
-      console.log("Button was pressed!")
       googleLogin();
     }
   }
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -19,4 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFireStore(firebaseApp)
+const db = getFirestore(app)
